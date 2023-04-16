e117c39238d7aafad9ba500425dfac222ab01ef2
Avinashjat <127595602+Avinashjat@users.noreply.github.com>
2023-04-16 17:45:43 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+let a = 10;
+let b = 4;
+
+//arithemetic operaters
+console.log("a+b =", a + b)
+console.log("a-b =", a - b)
+console.log("a*b =", a * b)
+console.log("a/b =", a / b)
+console.log("a**b =", a ** b)
+console.log("++a =", ++a)
+console.log("a++ =", a++)
+console.log("--a =", --a)
+console.log("a-- =", a--)
+console.log("a =", a)
+console.log("a-- =", a--)
+
+//assignment operaters
+
+let a1 = 6;
+let a2 = 4;
+a1 += 3
+a2 -= 3
+a1 *= 3
+
+console.log(a1,a2)
+
+//comparision operaters
+ let c1=6;
+let c2=7;
+let c3="6";
+console.log("c1==c2 is",c1==c2 )
+console.log("c1!=c2 is",c1!=c2 )
+console.log("c1===c3 is",c1===c3 )
+console.log("c1!==c3 is",c1!==c3 )
+
+//logical  operaters
+
+let  x=4;
+let y=5;
+console.log(x>y && x==4)
+console.log(x>y || x==4)
+console.log(!true)
+
+
+
+
